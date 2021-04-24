dd1fd356e011972b5978d3689269359e1151fd8a
Chris Carlisle <chris@chriscarlisle.uk>
2021-04-24 22:24:07 +0100

    First pass, happy path

--- lib/slowly_disemvowel.rb
+++ lib/slowly_disemvowel.rb
@@ -1,3 +1,16 @@
-def slowly_disemvowel
-  #
+def slowly_disemvowel(string)
+  strings = []
+
+  while contains_a_vowel(string) do
+    string = string.sub(/[aeiou]/i, '')
+    strings << string
+  end
+
+  strings
+
+end
+
+
+def contains_a_vowel(string)
+  return string =~ /[aeiou]/i
 end
--- test/test_slowly_disemvowel.rb
+++ test/test_slowly_disemvowel.rb
@@ -2,5 +2,9 @@ require "minitest/autorun"
 require_relative "../lib/slowly_disemvowel"
 
 class TestSlowlyDisemvowel < Minitest::Test
-  
+
+  def test_it_returns_an_array_of_disemvowelled_strings
+    assert_equal ["Mnitest", "Mntest", "Mntst"], slowly_disemvowel("Minitest")
+  end
+
 end
